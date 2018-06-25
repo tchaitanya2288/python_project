008a021ba0bb936533b2d161483c74b64b5ca8c2
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-06-25 17:31:48 -0400

    operators

---
+++ Python_Basics/comparators.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# == --> value equal to
+# >  --> Greatertahn
+#<   --> lessthan
+#>=  --> greaterthan equalto
+#<=  --> lessthan equalto
+
+bool_one = 10 == 10
+print(bool_one)
+less= 11 < 10
+print(less)
+grea = 12 > 10
+print(grea)
+less_equ = 12 <= 13
+print(less_equ)
+grea_equ = 12 >= 11
+print(grea_equ)
