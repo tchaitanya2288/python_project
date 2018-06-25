f48e63a930560ffec6e180bb28b7ea14bfcf2678
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-06-25 18:15:32 -0400

    operators1

---
+++ Boolean_operator/boolean_precedence.py
@@ -0,0 +1,10 @@
+"""
+1.not
+2.and
+3.or
+"""
+bool_output = True or not False and False
+#o/p --> true --> not false means true and false-->false-->true or false--->true
+bool_output1 = 10 == 10 or  not 10 >10 and 10 <9
+print(bool_output)
+print(bool_output1)
