80f155484462bfd9940c812f68f38a69b2fb8d51
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-06-25 18:39:23 -0400

    while_condition

---
+++ while_loop.py
@@ -0,0 +1,10 @@
+""""
+Execute statements repeatedly
+conditions are used to stop execution of loops
+Iterable items are strings,list,tuple,dictionaries
+"""
+x = 0
+while x<10:
+    print("value of x is:", x)
+    x= x+1
+
