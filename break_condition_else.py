de306b56cdd8b339988487234149e92dc8ce2414
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-06-25 18:52:24 -0400

    while_condition2

---
+++ break_condition_else.py
@@ -0,0 +1,13 @@
+"""
+break --> to Break out the closest enclosing loop
+continue --> go to the start of closest enclosing loop
+"""
+x = 0
+while x<10:
+    print("The value of x is:",x)
+    x += 1
+    if x == 8:
+        break
+    print("The example is awesome")
+    print('*'*20)
+print("come out of the loop")
