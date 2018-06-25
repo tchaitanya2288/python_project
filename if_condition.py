f902ce360a91bc30870739784a0ba0ade693a5c1
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-06-25 18:27:12 -0400

    logical_condition

---
+++ if_condition.py
@@ -0,0 +1,20 @@
+"""
+conditional logic
+"""
+if 100>10:
+    print("hundread greaterthan ten")
+
+value = 'green'
+
+if value == 'green':
+    print('go')
+else:
+    print('stop')
+
+value1 = 'yellow'
+if value1 == "green":
+    print('go')
+elif value1 == "yellow":
+    print('prepare to stop')
+else:
+    print('stop')
