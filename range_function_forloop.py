8e068633b207c789c4fc3c966c06f9aca12dfc73
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-06-26 10:08:04 -0400

    for_loop-type

---
+++ range_function_forloop.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""
+Range is built in function
+create a sequence of numbers but does not save them in memory
+very useful for generating numbers
+"""
+print(list(range(0,10)))
+
+a=range(10,20)
+print(a)
+print(type(a))
+print(list(a))
