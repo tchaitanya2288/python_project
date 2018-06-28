2ec7a87e658aa3b54af7546498434934c5912102
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-06-28 13:15:00 -0400

    class_ex

--- class_object/understanding_class_obj.py
+++ class_object/understanding_class_obj.py
@@ -5,14 +5,22 @@ _init_ -->  is a function or method used to intialize all the objects that we go
 
 class Car(object):
 
+    wheels = 4 #member variable
+
     def __init__(self,make,model):
         self.make = make
         self.model= model
-c1 = Car('bmw','550i')
-print(c1.make)
-print(c1.model)
 
+    def info(self):
+        print("make of the car:",self.make)
+        print("model of the car:",self.model)
 
+c1 = Car('Bmw','550i')
+c1.info()
+c2 = Car("Audi","330A")
+c2.info()
+
+print(Car.wheels)
 
 
 
