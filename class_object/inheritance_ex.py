b09d11348b9d1919835fae94b6f32ac63c3c5fec
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-06-28 13:53:54 -0400

    Inheritance_ex1

--- class_object/inheritance_ex.py
+++ class_object/inheritance_ex.py
@@ -8,6 +8,22 @@ class Car(object):
     def stop(self):
         print("car stopped....")
 
+class Bmw(Car):                    #child class
+    def __init__(self):
+        Car.__init__(self)
+        print("You just created Bmw car instance")
+    def drive(self):
+        super(Bmw,self)
+        print("You are driving Bmw enjoy...........")
+    def headsup_display(self):
+        print("This is unique feature of Bmw")
+
+
 c1 = Car()
 c1.drive()
 c1.stop()
+
+b = Bmw()
+b.drive()
+b.headsup_display()
+b.stop()
