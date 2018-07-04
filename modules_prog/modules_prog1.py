e7633514878df0fa982ddc488fc7d41c1c5231eb
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-07-04 09:44:42 -0400

    modules_ex1

---
+++ modules_prog/modules_prog1.py
@@ -0,0 +1,14 @@
+# https://docs.python.org/3/library
+
+import math
+from math import sqrt
+
+class modules_demo():
+
+    def builtin_modules(self):
+        print(math.sqrt(100))
+        print(sqrt(64))
+
+
+m = modules_demo()
+m.builtin_modules()
