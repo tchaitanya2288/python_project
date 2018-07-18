f1edac70289301159582d228e7ea9f0fbd3957fc
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-07-18 11:24:45 -0400

    for_loop

---
+++ Exercise/for_loop_ex.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+def say_hello_world_thrice():
+    print("Hello world")
+    print("Hello world")
+    print("Hello world")
+
+say_hello_world_thrice()
+
+print('$$$$$$$$$$$$$$$$$$')
+
+def sample_method():
+    print("I've created my first variable")
+    print("I've created my first loop")
+    print("I've created my first method")
+    print("I'm excited to learn python")
+
+sample_method()
