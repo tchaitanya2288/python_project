22da5c4bfb937f6b2ef9cd9f11c9762d57fa7644
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-07-18 10:43:43 -0400

    tables_prog

--- Exercise/6 table.py
+++ Exercise/6 table.py
@@ -7,3 +7,17 @@ print('********************************')
 
 for i in range(1,11):
     print(f"4 * {i} = {4 * i }")
+
+print("###############################")
+
+for i in range(10,0,-1):
+    print(i)
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+#To print odd numbers from 1 to 10
+for i in range(1,11,2):
+    print(i)
+print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+# To print even numbers from 1 to 10
+for i in range(2,11,2):
+    print(i)
