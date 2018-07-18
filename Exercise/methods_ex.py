6726eac8e7b05e208786979a10994fab8d383b59
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-07-18 15:47:18 -0400

    methods

---
+++ Exercise/methods_ex.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def sum_of_integers(a,b,c):
+    sum = a+b+c
+    print(sum)
+sum_of_integers(12,13,10)
+
+print("*****************")
+
+def angles_traingle(a,b):
+     c = 180-(a+b)
+     print(c)
+     print("The third angle values is:",c)
+angles_traingle(60,70)
