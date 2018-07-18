fb4bb6c5500aab42cc12b57a83a6cd2143fd6a3e
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-07-18 11:53:34 -0400

    tables_ex

---
+++ Exercise/multiplication_table_method.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# def multipication_table(table,start,end):
+#     for i in range(start,end+1):
+#         print(f"{table} * {i} = {table * i}")
+#
+# multipication_table(6,1,5)
+
+def multipication_table(table=11,start=1,end=10):
+    for i in range(start,end+1):
+        print(f"{table} * {i} = {table * i}")
+
+multipication_table()
+#multipication_table(15,1,6)
+
+
+
