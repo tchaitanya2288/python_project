62a1e08254703774d2c72c06be3b0e88c83f03cc
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-07-20 11:39:09 -0400

    methods

---
+++ Exercise/simple_if.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+a = 5
+b = 7
+
+if a>b:
+    print(f"{a} is greater than {b}")
+else:
+    print(f"{a} is less than {b}")
+
+print("&&&&&&&&&&&&")
+
+angle1 = 90
+angle2 = 30
+angle3 = 60
+if angle1+angle2+angle3 == 180:
+    print("Its forms a valid triangle")
