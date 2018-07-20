464be0a477344fd7eb9fd001fae380e212941087
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-07-20 14:59:48 -0400

    methods

---
+++ Exercise/sample12.py
@@ -0,0 +1,16 @@
+import string
+
+string_example = "This\nis\na\ngreat\nthing"
+
+print(string_example.split())
+print(string_example)
+print('@'*20)
+print('%'*10)
+
+for i in range(1,10):
+    print(i)
+
+print("#"*20)
+
+for i in "Hello World":
+    print(i)
