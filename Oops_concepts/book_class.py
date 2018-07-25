838313b4b153506b6bb4b938ddf2999e8686d929
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-07-25 11:30:19 -0400

    oops_concept

--- Oops_concepts/book_class.py
+++ Oops_concepts/book_class.py
@@ -1,7 +1,13 @@
 class Book:
-    def __init__(self,name):
+    def __init__(self,name,copies=0):
         self.name = name
+        self.copies = copies
 
+    def increase_copies(self, how_much):
+        self.copies += how_much
+
+    def decrease_copies(self, how_much):
+        self.copies -= how_much
 
 art_science = Book("The Art of Science")
 Python_programming = Book("THe Python Programm in 100 steps")
@@ -14,3 +20,8 @@ art_journals = Book("The Journals")
 print(art_journals.name)
 print(art_science.name)
 print(Python_programming.name)
+
+Python_programming.increase_copies(25)
+Python_programming.decrease_copies(10)
+
+print(Python_programming.copies)
