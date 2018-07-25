b10f850558035b342a999e034f8f249b7181fdda
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-07-25 09:40:30 -0400

    oops_concept

---
+++ Oops_concepts/book_class.py
@@ -0,0 +1,14 @@
+class Book:
+    pass
+
+art_science = Book()
+Python_programming = Book()
+art_journals = Book()
+
+art_science.name = "The Art of Science"
+Python_programming.name = "THe Python Programm in 100 steps"
+art_journals.name = "The Journals"
+
+print(art_journals.name)
+print(art_science.name)
+print(Python_programming.name)
