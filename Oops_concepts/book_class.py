37ebe9c4e5062c838b463557c529d8167506f466
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-07-25 10:08:15 -0400

    oops_concept

--- Oops_concepts/book_class.py
+++ Oops_concepts/book_class.py
@@ -1,14 +1,16 @@
 class Book:
-    pass
+    def __init__(self,name):
+        self.name = name
 
-art_science = Book()
-Python_programming = Book()
-art_journals = Book()
 
-art_science.name = "The Art of Science"
-Python_programming.name = "THe Python Programm in 100 steps"
-art_journals.name = "The Journals"
-
+art_science = Book("The Art of Science")
+Python_programming = Book("THe Python Programm in 100 steps")
+art_journals = Book("The Journals")
+#
+# art_science.name = "The Art of Science"
+# Python_programming.name = "THe Python Programm in 100 steps"
+# art_journals.name = "The Journals"
+#
 print(art_journals.name)
 print(art_science.name)
 print(Python_programming.name)
